88dd45d333cfcb4dfc4676cc61ff84936780feb2
Falmanza94 <fischer.almanza@gmail.com>
2025-04-22 16:18:52 -0500

    userSchema has nearly been initialized

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,2 +1,39 @@
-import { Schema, model, Types } from 'mongoose';
+import { Schema, Document, model, Types } from 'mongoose';
 
+export interface IUser extends Document {
+    username: string;
+    email: string;
+    thoughts: Types.ObjectId[];
+    friends: Types.ObjectId[];
+    friendCount?: number;
+}
+
+const userSchema = new Schema<IUser>(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Enter a valid email address'],
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought',
+        },
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      ],
+    },
+    
+)
